8d79f754aa9824c6bb79573acfdd4ca04d4ecf7a
ludrob <robin.ludovic@gmail.com>
2018-11-02 11:38:13 +0000

    typo  🐿 v2.10.3

--- src/config/settings.js
+++ src/config/settings.js
@@ -5,5 +5,5 @@ module.exports = {
         `${staticAssetsBasePath}/break_out.jpg`,
         `${staticAssetsBasePath}/audiance_clapping.jpg`
     ],
-    'magnetDataSourceUrl': 'g/magnet/api'
+    'magnetDataSourceUrl': '/magnet/api'
 };
