0db9b2faf648aa86fb4d5c1889cf92a281ad8e88
Ahmad M <ahmadmoawad2@yahoo.com>
2021-08-12 14:37:58 +0200

    update segmentid

--- src/components/eventpromo/eventpromo-utils.js
+++ src/components/eventpromo/eventpromo-utils.js
@@ -1,6 +1,8 @@
 import ftDateFormat from '@financial-times/ft-date-format';
 
-export function getFormattedDate(event) {
+const segmentId = 'a3ced873-2d86-959c-46de-b9eab4c8f8d0';
+
+export function getFormattedDate (event) {
 	if (!event.scheduledStartTime || !event.scheduledEndTime) return;
 	const year = ftDateFormat.format(event.scheduledStartTime, 'yyyy');
 	const eventStart = ftDateFormat.format(event.scheduledStartTime, 'dd MMMM');
@@ -12,9 +14,9 @@ export function getFormattedDate(event) {
 	}
 }
 
-export function getMappedData(event) {
+export function getMappedData (event) {
 	const eventUrl = new URL(event.eventUrl);
-	eventUrl.searchParams.set('segmentId', 'a3ced873-2d86-959c-46de-b9eab4c8f8d0');
+	eventUrl.searchParams.set('segmentId', segmentId);
 	return {
 		id: event.id,
 		brand: event.brand,
@@ -24,6 +26,6 @@ export function getMappedData(event) {
 		location: event.location,
 		link: eventUrl.toString(),
 		imageUrl: event.imageUrl,
-		segmentId: event.segmentId
+		segmentId: segmentId
 	};
 }
--- src/components/forumpromo/main.js
+++ src/components/forumpromo/main.js
@@ -2,20 +2,22 @@
 import {Forumpromo} from '@financial-times/n-eventpromo';
 import {h, render} from '@financial-times/x-engine';
 import {dispatchTrackingEvent} from '../../lib/tracking';
-import {getMappedData} from './eventpromo-utils';
+
+const segmentId = 'a3ced873-2d86-959c-46de-b9eab4c8f8d0';
 
 export function renderForumpromo (magnetPlaceholderSelector, data) {
 	try {
-		const formattedData = getMappedData(data);
-		const promoElement = <Forumpromo {...formattedData} />;
+		const forumUrl = new URL(data.link);
+		forumUrl.searchParams.set('segmentId', segmentId);
+		const promoElement = <Forumpromo {...data} />;
 		render(promoElement, magnetPlaceholderSelector);
 
 		// tracking
 		dispatchTrackingEvent({
 			category: 'n-forumpromo',
 			action: 'shown',
-			forumId: formattedData.id,
-			segmentId: formattedData.segmentId
+			forumId: data.id,
+			segmentId: segmentId
 		});
 	} catch (err) {
 		err.message = `failed to render forumpromo, cause: ${err.message}`;
