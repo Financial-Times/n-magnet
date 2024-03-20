089c9e5f0e9c562b4419967a3849cf4f2e5e72a8
Luke Kavanagh <kavanagh@users.noreply.github.com>
2024-03-20 13:53:12 +0000

    Make prettier work
    It had been added to thew project via Toolkit but it was not working.

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	singleQuote: true,
+	quoteProps: 'as-needed',
+	bracketSpacing: true,
+	bracketSameLine: true,
+	trailingComma: 'all',
+	arrowParens: 'always',
+	semi: false,
+	editorconfig: true,
+	useTabs: true,
+	printWidth: 180,
+}
